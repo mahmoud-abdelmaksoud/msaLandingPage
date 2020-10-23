be29575d6c03c19003b8fa9345b78d24194bd859
mahmoud <m.saad.abdelmaksoud@gmail.com>
2020-10-23 22:19:03 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BlogComponent } from "./pages/blog/blog.component";
+import { HomeComponent } from "./pages/home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "blog", component: BlogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./pages/home/about/about.component";
+import { ServicesComponent } from "./pages/home/services/services.component";
+import { PortfolioComponent } from "./pages/home/portfolio/portfolio.component";
+import { TeamComponent } from "./pages/home/team/team.component";
+import { BlogComponent } from "./pages/blog/blog.component";
+import { ContactComponent } from "./pages/home/contact/contact.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeroComponent } from "./pages/home/hero/hero.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    TeamComponent,
+    BlogComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
